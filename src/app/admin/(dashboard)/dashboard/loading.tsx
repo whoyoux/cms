718a5ae417451e37e608e0dd375edf406b182cf7
whoyoux <48690153+whoyoux@users.noreply.github.com>
2025-08-23 15:21:30 +0200

    Update loading.tsx

--- src/app/admin/(dashboard)/dashboard/loading.tsx
+++ src/app/admin/(dashboard)/dashboard/loading.tsx
@@ -1,5 +1,11 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 const AdminDashboardPageLoading = () => {
-    return <div>Loading ...</div>;
+    return (
+        <div className="w-full p-4">
+            <Skeleton className="min-h-[40dvh] bg-gray-200" />
+        </div>
+    );
 };
 
 export default AdminDashboardPageLoading;
