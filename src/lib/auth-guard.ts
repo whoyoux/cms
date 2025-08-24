e1776e4d95edf8c77b46106014b8bc20b06f5560
whoyoux <48690153+whoyoux@users.noreply.github.com>
2025-08-24 11:25:05 +0200

    Update auth-guard.ts

--- src/lib/auth-guard.ts
+++ src/lib/auth-guard.ts
@@ -1,11 +1,13 @@
 import "server-only";
 
 import { redirect } from "next/navigation";
+import { ROUTES } from "@/constants/routes";
 import { getCachedSession } from "./get-session";
 
 export const authGuard = async () => {
     const data = await getCachedSession();
 
-    if (!data.session || !data.isLoggedIn) return redirect("/admin/sign-in");
+    if (!data.session || !data.isLoggedIn)
+        return redirect(ROUTES.ADMIN.SIGN_IN);
     return data;
 };
